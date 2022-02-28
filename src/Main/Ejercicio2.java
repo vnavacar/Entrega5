973ad6bcc135302d66f5995ec1ad2c442a174c0b
vnavacar <vnavacar@myuax.com>
2022-02-28 18:31:06 +0100

    ejercicio 2 casi hecho

--- src/Main/Ejercicio2.java
+++ src/Main/Ejercicio2.java
@@ -7,6 +7,7 @@ public class Ejercicio2 {
 		
 		ArrayList<Integer> array = new ArrayList<Integer>();
 		int numero;
+		int cuenta;
 		
 		System.out.println("Indicar un valor comprendido entre 0 y 9 o -1 para terminar:");
 		do{
@@ -14,12 +15,24 @@ public class Ejercicio2 {
 			
 			if(numero != -1) {
 				array.add(numero);
+				System.out.println("Otro valor, por favor o -1 para terminar...");
 			}
 			
 			
 		}while(numero != -1);
 		
 		
+		for(int i=0;i<10;i++) {
+			cuenta =0;
+			for(int j=0;j<array.size();j++) {
+				if(array.get(j)==i) {
+					cuenta++;
+				}
+				
+				
+			}
+			
+		}
 		
 		
 		
