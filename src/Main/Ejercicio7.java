e46cce814e389e9c599391bb6b5255d212a3356d
vnavacar <vnavacar@myuax.com>
2022-03-01 15:52:45 +0100

    Create Ejercicio7.java

---
+++ src/Main/Ejercicio7.java
@@ -0,0 +1,15 @@
+package Main;
+import java.util.Random;
+
+public class Ejercicio7 {
+	
+	public static void Lanzar() {
+		
+	    Random r = new Random();
+		
+		int[][] arr2d = new int[20][30];
+		
+		
+	}
+
+}
