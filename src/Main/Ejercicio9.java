c3a965f232e41d9b275ac56863a0fe06a513ecaf
vnavacar <vnavacar@myuax.com>
2022-03-01 18:08:50 +0100

    Update Ejercicio9.java

--- src/Main/Ejercicio9.java
+++ src/Main/Ejercicio9.java
@@ -6,8 +6,58 @@ public class Ejercicio9 {
 	public static void Lanzar(Scanner teclado) {
 		
 		
+		char[][] arr2d = new char[3][3];
 		
+		boolean jugador = false; // false para jugador , true para 2
+		boolean gameOver = false;
+		int fila;
+		int columna;
 		
+		for(int i=0;i<3;i++) {
+			
+			for(int j=0;j<3;j++) {
+				
+				arr2d[i][j]='-';
+				
+				}
+			}
+		Imprimirtabla(arr2d);
+		System.out.println("Juguemos a 3 en raya!");
+		
+		do {
+			if(jugador) {
+				System.out.println("Turno del jugador 1(X)");
+			}else {
+				System.out.println("Turno del jugador 2(O)");
+			}
+			System.out.println("Que fila?");
+			fila = teclado.nextInt() -1;
+			System.out.println("Que columna?");
+			columna = teclado.nextInt() -1;
+			if(jugador) {
+				arr2d[fila][columna]='X';
+			}else {
+				arr2d[fila][columna]='O';
+			}
+			Imprimirtabla(arr2d);
+			
+			
+			}while(!gameOver);
+		
+	}
+	
+	private static void Imprimirtabla(char[][] arr2d)
+	{
+		
+		for(int i=0;i<3;i++) {
+			
+			for(int j=0;j<3;j++) {
+				
+				System.out.printf(arr2d[i][j] + "");
+				
+				}
+			System.out.println();
+			}
 		
 	}
 
